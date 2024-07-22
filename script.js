1607975428cb28f3da416aa8a980de33243ed9f3
SashankASC24 <sashank.sat@gmail.com>
2024-07-22 15:13:30 -0500

    sync

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Add your code to this file
+let color = document.querySelector("body");
+color.style.backgroundColor = ("rgb(0,0,139)");
+
+let img1 = document.querySelector("img");
+img1.style.border = ("dotted, orange");
+
+
+let imgs = document.querySelectorAll(".pokemon_images");
+imgs.style.border = ("dotted, orange");
